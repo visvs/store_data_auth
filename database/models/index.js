23cd4a0ed9fdb35fb20ac00a64be2b8636185a8f
Violeta <LIS17110640@irapuato.tecnm.mx>
2022-04-04 19:45:50 -0500

    feat: Añadida nueva entidad customer: service, model, squema y routes

--- database/models/index.js
+++ database/models/index.js
@@ -1,6 +1,8 @@
 const {User, userSchema} = require('./user.model');
+const {Customer, CustomerSchema} = require('./customer.model');
 
 function setupModels(sequelize){
   User.init(userSchema, User.config(sequelize))
+  Customer.init(CustomerSchema, Customer.config(sequelize))
 }
 module.exports = setupModels;
