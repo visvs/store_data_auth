45cf305201085f52b4e11df4b0ee6153e08133b4
Violeta <LIS17110640@irapuato.tecnm.mx>
2022-04-07 18:02:50 -0500

    fix: migration error

--- database/migrations/20220406203444-order-table.js
+++ database/migrations/20220406203444-order-table.js
@@ -1,11 +1,41 @@
 'use strict';
 
-const {OrderSchena, ORDER_TABLE} = require('../models/order.model')
+const { ORDER_TABLE} = require('../models/order.model')
+const {DataTypes, Sequelize} = require('sequelize');
+const { CUSTOMER_TABLE } = require('../models/customer.model');
 
 module.exports = {
   async up (queryInterface) {
     //queryInterface nos permite crear las tablas
-    await queryInterface.createTable(ORDER_TABLE,OrderSchena)
+    await queryInterface.createTable(ORDER_TABLE,{
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      status: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },})
   },
 
   async down (queryInterface) {
