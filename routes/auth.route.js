581c61e5da777fc2491e6e36c25ceaa4d885298c
Violeta <LIS17110640@irapuato.tecnm.mx>
2022-04-14 11:20:19 -0500

    feat: ruta donde se genera el login del usuario y se obtine el JWT y se establece el body del payload

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const {config} = require('../config/config');
 
 const router = express.Router();
 
 router.post('/login', passport.authenticate('local', {session: false}), async (req, res, next) => {
   try {
-    res.json(req.user);
+    const user = req.user;
+    const payload = {
+      sub: user.id,
+      role: user.role
+    }
+    const token = jwt.sign(payload, config.JWT_secret);
+    res.json({user,token });
   } catch (error) {
     next(error);
   }
